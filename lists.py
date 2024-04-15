9e944941c0abe80b48f279e5161c35166bf09b84
Ali-Mwanasha <alimwanashaomari@gmail.com>
2024-04-15 22:57:38 +0300

    more list questions

--- lists.py
+++ lists.py
@@ -63,3 +63,43 @@ if "Iceland" in nordiccountry:
     print("Iceland is not a nordic country")
 
     
+# The following is a list of 10 students ages:
+ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+    
+
+
+
+# Sort the list and find the min and max age
+ages.sort()
+print(ages)
+
+print("min=",min(ages))
+print("max=",max(ages))
+
+# Add the min age and the max age again to the list
+ages.insert(1,22.8)
+print(ages)
+ages.insert(2,24.0)
+print(ages)
+
+
+# Find the median age (one middle item or two middle items divided by two)
+print("The original list : " + str(ages)) 
+ages.sort() 
+mid = len(ages) // 2
+res = (ages[mid] + ages[~mid]) / 2
+print( str(res)) 
+# Find the average age (sum of all items divided by their number )
+    
+average = sum(ages)/len(ages)
+
+print(round(average,1))
+
+
+# Find the range of the ages (max minus min)
+
+
+
+# Compare the value of (min - average) and (max - average), use abs() method
+    
+
